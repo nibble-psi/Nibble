b74e1daf39ab3644bb050b33ff11a3702f23bc3c
Mantas Jasikėnas <mantas.jasikenas@ktu.edu>
2023-03-14 21:15:28 +0200

    Refactored code. Added icons.

--- NibbleTools/ViewModels/NotOperationViewModel.cs
+++ NibbleTools/ViewModels/NotOperationViewModel.cs
@@ -1,36 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using NibbleTools.Helpers;
 
 namespace NibbleTools.ViewModels;
 
 public partial class NotOperationViewModel : ObservableObject
 {
-    //[ObservableProperty]
-    //private int number = 0x00000000;
-
-
-    //public NotOperationViewModel()
-    //{
-
-    //}
-
-    //[RelayCommand]
-    //private void IncreseNumber()
-    //{
-    //    Number++;
-    //}
-
-
-    //public int Not(int number)
-    //{
-    //    return ~number;
-    //}
-
     [ObservableProperty]
     private uint _firstValue = 0;
 
     [ObservableProperty]
-    private uint _result = 0;
+    private string _result = string.Empty;
 
 
     public NotOperationViewModel()
@@ -40,7 +20,7 @@ public partial class NotOperationViewModel : ObservableObject
 
     partial void OnFirstValueChanged(uint value)
     {
-        Result = Not(value);
+        Result = Not(value).ToBinaryString();
     }
 
 
