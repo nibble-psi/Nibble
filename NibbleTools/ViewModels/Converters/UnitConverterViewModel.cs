dcbe67f94b383516b233f2fbded303e287f1e3a9
Mantas <mantas.smulkstys@ktu.edu>
2023-04-17 23:53:26 +0300

    NIB-17-Unit-converter

---
+++ NibbleTools/ViewModels/Converters/UnitConverterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace NibbleTools.ViewModels;
+public partial class UnitConverterViewModel : ObservableRecipient
+{
+    [ObservableProperty]
+    private string _convertFrom = string.Empty;
+
+    [ObservableProperty]
+    private string _convertTo = string.Empty;
+
+    [ObservableProperty]
+    private double _value = 0;
+
+    [ObservableProperty]
+    private double _result = 0;
+}
---
+++ NibbleTools/Views/Converters/UnitConverter.xaml.cs
@@ -0,0 +1,72 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
+using NibbleTools.ViewModels;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace NibbleTools.Views;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class UnitConverterPage : Page
+{
+    public UnitConverterPage()
+    {
+        ViewModel = App.GetService<UnitConverterViewModel>();
+        DataContext = ViewModel;
+        this.InitializeComponent();
+    }
+
+    public UnitConverterViewModel ViewModel
+    {
+        get;
+        set;
+    }
+
+    public void Convert_Click(object sender, RoutedEventArgs e)
+    {
+        var selectedOne = itemOne.SelectedItem as ComboBoxItem;
+        var selectedTwo = itemTwo.SelectedItem as ComboBoxItem;
+        var value = Value.Value;
+
+        var from = selectedOne.Content as string;
+        var to = selectedTwo.Content as string;
+
+        var factors = new Dictionary<(string, string), double>
+    {
+        { ("m", "km"), 0.001 },
+        { ("m", "cm"), 100 },
+        { ("km", "m"), 1000 },
+        { ("km", "cm"), 100000 },
+        { ("cm", "m"), 0.01 },
+        { ("cm", "km"), 0.00001 }
+    };
+        if (factors.ContainsKey((from, to)))
+        {
+            var factor = factors[(from, to)];
+            var x = Convert.ToString(value * factor);
+
+            TextBox1.Text = x;
+        }
+        else
+            TextBox1.Text = Convert.ToString(value);
+
+    }
+}
